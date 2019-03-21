a0d77f4122c7e9fe049ca2ef06e98ab70463fde9
Tyson <tyson.cross@gmail.com>
2019-03-21 16:19:21 +0200

    Simulink Model Added transfer function Adjusted Model paramaters

--- matlab/NewtonMethod.m
+++ matlab/NewtonMethod.m
@@ -5,7 +5,7 @@
 clc; clear all;
 
 % System Parameters
-m = 0.1;                                            % kg
+m = 0.2;                                           % kg
 B = 0.2;                                            % Telsa
 l = 0.6;                                            % m
 R = 5;                                              % Ohms
@@ -17,18 +17,27 @@ syms s t
 num = 1;                                            % numerator of TF
 s1 = l.*B + f.*R./(l.*B);                           % s term of 2nd order ODE
 s2 = m.*(R./(l.*B));                                % s^2 term of 2nd order ODE
-den = s2.*(s.*s) + s1.*s ;                          % denominator of TF
-sys = num./den;                                     % TF
+sden = s2.*(s.*s) + s1.*s ;                          % denominator of TF
+
+sys = num./sden;                                     % TF as symbolic function
 X = Vs.*sys;                                        % output = input * TF
 
-xt = ilaplace(X);                                   % inverse laplace
-t = 0:0.1:10;                                       % time series input
-x = subs(xt);                                       % explicit values
+% % Solution
+% xt = ilaplace(X);                                   % inverse laplace
+% 
+% % Data generation for NN
+% t = 0:0.1:10;                                       % time series input
+% x = subs(xt);                                       % explicit values
 
-% NN parameters
-input = repmat(Vs,1,length(t));
-output = double(x);
+% % NN parameters
+% input = repmat(Vs,1,length(t));
+% output = double(x);
 
-% system = tf(num,den);
-%ss_system = tf2ss(system.num,system.den);
-%system = tf([(B.*L),],[1])
+% Simulink
+den = sym2poly(sden);
+system = tf(num, den);
+rlocus(system)
+tau = 50;
+
+% % state space
+% ss_system = tf2ss(num, den);
