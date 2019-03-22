4662e65e9815d636d0988f5a62dad964e141843f
Tyson <tyson.cross@gmail.com>
2019-03-22 10:15:52 +0200

    Adjusted feedback to speed up response

--- matlab/NewtonMethod.m
+++ matlab/NewtonMethod.m
@@ -5,9 +5,9 @@
 clc; clear all;
 
 % System Parameters
-m = 0.2;                                           % kg
+m = 0.1;                                            % kg
 B = 0.2;                                            % Telsa
-l = 0.6;                                            % m
+l = 1;                                              % m
 R = 5;                                              % Ohms
 Vs = [-20:0.1:20]';                                 % volts (input range)
 f = 0.01;                                           % friction coefficient
@@ -17,10 +17,19 @@ syms s t
 num = 1;                                            % numerator of TF
 s1 = l.*B + f.*R./(l.*B);                           % s term of 2nd order ODE
 s2 = m.*(R./(l.*B));                                % s^2 term of 2nd order ODE
-sden = s2.*(s.*s) + s1.*s ;                          % denominator of TF
+sden = s2.*(s.*s) + s1.*s ;                         % denominator of TF
 
-sys = num./sden;                                     % TF as symbolic function
+sys = num./sden;                                    % TF as symbolic function
 X = Vs.*sys;                                        % output = input * TF
+
+% Simulink
+den = sym2poly(sden);
+system = tf(num, den);
+rlocus(system)
+tau = 1;
+
+% % state space
+% ss_system = tf2ss(num, den);
 
 % % Solution
 % xt = ilaplace(X);                                   % inverse laplace
@@ -32,12 +41,3 @@ X = Vs.*sys;                                        % output = input * TF
 % % NN parameters
 % input = repmat(Vs,1,length(t));
 % output = double(x);
-
-% Simulink
-den = sym2poly(sden);
-system = tf(num, den);
-rlocus(system)
-tau = 50;
-
-% % state space
-% ss_system = tf2ss(num, den);
