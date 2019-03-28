9e0318ab2874afde27d31deb3de48b0d1f24bdee
James <goodhead.j@gmail.com>
2019-03-28 17:03:30 +0200

    Added non linear characteristics for the resistance

--- matlab/NewtonMethod.m
+++ matlab/NewtonMethod.m
@@ -5,10 +5,10 @@
 clc; clear all;
 
 % System Parameters
-m = 0.1;                                            % kg
-B = 0.2;                                            % Telsa
-l = 1;                                              % m
-R = 0.2;                                            % Ohms
+m = 0.04;                                            % kg
+B = 0.3;                                            % Telsa
+l = 0.1;                                              % m
+R = 0.1;                                            % Ohms
 %Vs = [-20:0.1:20]';                                % volts (input range)
 Vs = 15
 f = 0.01;                                           % friction coefficient
@@ -21,7 +21,7 @@ s1 = l.*B;                                          % Without friction
 s2 = m.*(R./(l.*B));                                % s^2 term of 2nd order ODE
 sden = s2.*(s.*s) + s1.*s ;                         % denominator of TF
 sys = num./sden                                     % TF as symbolic function
-X = Vs.*sys;                                        % output = input * TF
+%X = Vs.*sys;                                        % output = input * TF
 
 % Modelling in terms of velocity
 % num = 1;
@@ -29,15 +29,82 @@ X = Vs.*sys;                                        % output = input * TF
 % sys = num./sden
 % velocity = Vs.*sys;
 
+
 % Simulink
 den = sym2poly(sden);
 system = tf(num, den);
-rlocus(system)
-tau = 1;
+%rlocus(system)
+%tau = 1;
 
-% % state space
-% ss_system = tf2ss(num, den);
+% Modelling of PID controller
+ P = 0.159243539750473
+ I = 0.0209333117982786
+ D = 0.242558878110515
+ N = 20.119
+PIDsystem = P + I/s + D*(N/(1+(N/s)))
 
+
+
+% Model of entire system 
+ Gs = PIDsystem.*sys
+ Ys = ((1/s)*Gs)/(1+Gs)
+ Distance = ilaplace(Ys)
+ Velocity = diff(Distance);
+ Acceleration = diff(Velocity);
+ %t = 0:0.01:5
+ 
+%  %Model of the Output of PID controler
+% controllerEffort = ((1/s)-Ys)*(PIDsystem)                     %Voltage applied to the motor
+% MotorV = (ilaplace(controllerEffort,t))
+% MotorC = MotorV./R                                            %Curent in the motor 
+% MotorP = MotorC.*MotorV                                        %instantanious Power
+% MotorEC = double(int(MotorP,t,0,3));
+%                                                               %Motor cumulative energy
+%                                                               
+% %Culculate the increase in resistace due to increase in temperature. 
+% deltaT = MotorEC/(0.385*0.75264);
+% Rnew = R*(1+0.0039*(deltaT));
+% 
+% 
+%  
+% %Plot system Mechanical Parameters
+%  figure
+%  fplot(Distance,[0 3])
+%  hold on
+%  fplot(Velocity,[0 3])
+%  hold on
+%  fplot(Acceleration/10,[0 3])
+%  title('Step Responce of Closed Loop System Including PID Controller');
+%  xlabel('Time (s)');
+%  ylabel('Displacement,Velocity,Acceleration (m,m/s,m/(s^2)x10^+1');
+%  
+%  %Plot system Electrical Parameters
+%  figure
+%  fplot(MotorV,[0 3])
+%  hold on
+%  fplot(MotorC,[0 3])
+%  hold on
+%  fplot(MotorP/10,[0 3])
+%  %hold on
+%  %fplot(MotorCE,[0 3])
+%  title('Step Responce of Closed Loop System Including PID Controller');
+%  xlabel('Time (s)');
+%  ylabel('Voltage,Current,Power, Energy (V,A,Wx10+1, J');
+%  legend('Voltage', 'Current','Power');
+ 
+ 
+
+
+% % state space modeling  controllable and observable
+
+ [Ass,Bss,Css,Dss] = tf2ss(num, den)
+ [V,D] = eig(Ass)
+ test = [0,1;-2,-3]
+ [V,D] = eig(test)
+ P = inv(V)
+ Lambda = P*Ass*inv(P)
+ Bhat = P*Bss
+ Chat = Css*inv(P)
 % % Solution
 % xt = ilaplace(X);                                   % inverse laplace
 % 
