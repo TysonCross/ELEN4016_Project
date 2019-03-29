252aecd03f98a3f18df4673377aa6b577435002a
Tyson <tyson.cross@gmail.com>
2019-03-29 14:07:40 +0200

    Smaller NN data set

--- matlab/NN_generate_input_data.m
+++ matlab/NN_generate_input_data.m
@@ -1,18 +1,18 @@
 % % Data generation for NN
 clear all;
 
-delta = 0.5;
-V = [-100:delta:100];                              	% volts (input range)
-t = linspace(0,10,length(V))';                      % time series input
-if (length(t) == length(V)) 
-    disp(["Length is:", length(t)]);
-end
+time_delta = 0.1;
+voltage_steps = 0.5;
+V = [-50:voltage_steps:50];                     % volts (input range)
+t = [0:time_delta:10]';                         % time series input
 
 % NN parameters
 System_parameters;
 b = f;
 B2 = B;
 B3 = B;
+
+% create matrix input
 v_1 = zeros(1,length(V));
 v_2 = repmat(V,length(t)-1,1);
 v_input = [v_1;v_2];
@@ -21,19 +21,23 @@ v_input = [v_1;v_2];
 % simulation
 start_simulink;
 model =  'NeuralNetUnitLDC_IO.slx';
-output = 0;
+% output = 0;
 load_system(model);
 
-for i = 1:length(t)
+for i = 1:length(V)
     input = timeseries(v_input(:,i),t);
     sim(model);
-%     sim(model,'StartTime',string(t(1)),'StopTime',string(t(end)),'FixedStep', string(t(2)-t(1)) );
+%     sim(model,'StartTime',string(t(1)),'StopTime',string(t(end)),'FixedStep', string(time_delta),'ReturnWorkspaceOutputs','on' );
     out_temp(:,i) = output.Data;
 end
 
 % after simulation
 in_NN_data = v_input;
 out_NN_data = out_temp;
+
+clear b B B2 B3 f i l m R input output out_temp model T t time_delta tout V v_1 v_2 v_input voltage_steps
+disp("Data generated")
+beep;
 
 % plot(t,in_NN_data(:,2));
 % hold on;
